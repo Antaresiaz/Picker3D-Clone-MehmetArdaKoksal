df23e129ed1f5732daed9051d388c5100c1269f4
Antaresiaz <markaard01@gmail.com>
2022-11-25 14:08:23 +0300

    Collecting and counting the balls have been improved.

--- Picker3D/Assets/GameManager.cs
+++ Picker3D/Assets/GameManager.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
+
+
+[Serializable]
+
+public class BallPoolMechanics
+{
+
+    public Animator BallPoolElevator;
+    public TextMeshProUGUI NumText;
+    public int RequiredBallCount;
+
+}
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject pickerObject;
+    [SerializeField] private GameObject BallControlObject;
     [SerializeField] private bool pickerMovementStatus;
+
+
+    int collectedBallCount;
+    [SerializeField] private  List<BallPoolMechanics> _BallPoolMechanics = new List<BallPoolMechanics>();
+
+
+
     void Start()
     {
         pickerMovementStatus = true;
+        _BallPoolMechanics[0].NumText.text = collectedBallCount + "/" + _BallPoolMechanics[0].RequiredBallCount;
     }
 
     
@@ -42,6 +65,31 @@ public class GameManager : MonoBehaviour
     public void BorderReach()
     {
         pickerMovementStatus = false;
-        Debug.Log("reached");
+
+
+        Collider[] HitColl = Physics.OverlapBox(BallControlObject.transform.position,BallControlObject.transform.localScale / 2,Quaternion.identity);
+
+
+        int i = 0;
+        while(i < HitColl.Length)
+        {
+            HitColl[i].GetComponent<Rigidbody>().AddForce(new Vector3(0,0,.8f),ForceMode.Impulse);
+            
+                i++;
+            
+
+            
+        }
+
+        Debug.Log(i);
+            
     }
+
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(BallControlObject.transform.position,BallControlObject.transform.localScale);
+    }
+    
 }
